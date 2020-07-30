a41d5212ae907b1d6420d425e2795140eadab431
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-07-30 10:43:25 -0600

    Removed depreciated prop info files.

--- dev/test.py
+++ dev/test.py
@@ -4,28 +4,28 @@ import pyprop
 opter = pyprop.Optimizer()
 
 # Perform random search
-#opter.random_search(n_units=100,
-#                    airspeed=20.0,
-#                    goal_type="thrust_to_weight_ratio",
-#                    goal_val=0.3,
-#                    airframe_weight=1.0,
-#                    filename="search_results.txt",
-#                    prop_constraints={"prop_type" : "data"},
-#                    motor_constraints={"Kv" : [200, 1000]})
-opter.random_search(n_units=1000,
+opter.random_search(n_units=100,
                     airspeed=20.0,
-                    goal_type="power_to_weight_ratio",
-                    goal_val=70.0,
+                    goal_type="thrust_to_weight_ratio",
+                    goal_val=0.3,
                     airframe_weight=1.0,
-                    filename="power_search_results.txt",
-                    prop_constraints={
-                        "prop_type" : "data",
-                        "diameter" : [17, 21],
-                        "pitch" : [6, 12]
-                    },
-                    motor_constraints={
-                        "Kv" : [100, 550]
-                    })
+                    filename="search_results.txt",
+                    prop_constraints={"prop_type" : "data"},
+                    motor_constraints={"Kv" : [200, 1000]})
+#opter.random_search(n_units=1000,
+#                    airspeed=20.0,
+#                    goal_type="power_to_weight_ratio",
+#                    goal_val=70.0,
+#                    airframe_weight=1.0,
+#                    filename="power_search_results.txt",
+#                    prop_constraints={
+#                        "prop_type" : "data",
+#                        "diameter" : [17, 21],
+#                        "pitch" : [6, 12]
+#                    },
+#                    motor_constraints={
+#                        "Kv" : [100, 550]
+#                    })
 
 ## Jaden's propulsion unit
 ##motor = pyprop.Motor(name="EFLITE Power 32", Kv=770, resistance=0.02, I_no_load=2.4, weight=7.0)
